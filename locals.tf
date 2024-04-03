4d7d7531c10fd0cc9b0d93db0fddee8f75cae3dc
kirankolasani <kirankumark906@gmail.com>
2024-04-03 17:54:44 +0530

    ansible files

--- locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-    ec2_name = merge(var.tags_roboshop, {Name = "roboshop_TF"})
-    sg_tags_name = merge(var.tags_roboshop, {Name = "Roboshop_Allow_ALL_TF"})
-
-    ec2_key_name = file("${path.module}/id_rsa.pub")
-    instance_count = length(var.instance_name)
-    
-  
-}
--- route53.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_route53_record" "route53" {
-  count = local.instance_count
-  zone_id = var.zone_id
-  name    = "${var.instance_name[count.index]}.${var.domain}"
-  type    = "A"
-  ttl     = 1
-  records = [ var.instance_name[count.index] == "web" ? aws_instance.tf-instance[count.index].public_ip : aws_instance.tf-instance[count.index].private_ip]
-}
