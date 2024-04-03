1ea776603d6cb37bf421a56818493643e9a7a03f
kirankolasani <kirankumark906@gmail.com>
2024-04-03 21:33:54 +0530

    ansible files

--- Session-02/README.md
+++ Session-02/README.md
@@ -1,3 +1,5 @@
 for_each
 
 dynamic block
+
+state & remote state using s3 & DynamoDB
--- Session-02/provider.tf
+++ Session-02/provider.tf
@@ -5,7 +5,14 @@ terraform {
       version = "~> 5.0"
     }
   }
+  backend "s3" {
+    bucket = "roboshop-state-file-s3"
+    key = "roboshop-state"
+    dynamodb_table = "roboshop-remote-state-lock"
+  
+ }
 }
+
 
 # Configure the AWS Provider
 provider "aws" {
